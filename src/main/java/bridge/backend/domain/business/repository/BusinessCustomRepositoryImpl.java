2bb97eb6f84d5f0a87af8a2068eecc1825cf827a
wiishj <inging007@naver.com>
2024-07-05 18:32:05 +0900

    update : dDay

--- src/main/java/bridge/backend/domain/business/repository/BusinessCustomRepositoryImpl.java
+++ src/main/java/bridge/backend/domain/business/repository/BusinessCustomRepositoryImpl.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 public class BusinessCustomRepositoryImpl implements BusinessCustomRepository {
-    private JPAQueryFactory queryFactory;
+    private final JPAQueryFactory queryFactory;
 
 
     @Override
